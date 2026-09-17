fa1ba0b883405838595b961cad122f429800ae2a
varshit1231 <chaudharyvarshit31@gmail.com>
2026-09-17 15:03:44 +0530

    Time: 4 ms (82.56%) | Memory: 42.2 MB (31.99%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+
+        long l = 0;
+        long r = (long) Math.sqrt(c);
+
+        while (l <= r) {
+            long sum = l * l + r * r;
+
+            if (sum == c)
+                return true;
+            else if (sum < c)
+                l++;
+            else
+                r--;
+        }
+
+        return false;
+    }
+}
